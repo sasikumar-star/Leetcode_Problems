0cc2045bf5e0e40f31e2bc62276d2fcd52440002
CHINTADA SASI KUMAR <sasikumarchintada64@gmail.com>
2025-12-15 23:03:21 +0530

    Time: 0 ms (100.00%) | Memory: 9.4 MB (76.49%) - LeetSync

---
+++ 541-reverse-string-ii/reverse-string-ii.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    string reverseStr(string s, int k) {
+        int n = s.size();
+
+        for(int i = 0; i < n; i += 2*k) {
+
+            int l = i;
+            int r = min(i + k - 1, n - 1);
+
+            while(l < r){
+                swap(s[l], s[r]);
+                l++;
+                r--;
+            }
+        }
+        return s;
+    }
+};
