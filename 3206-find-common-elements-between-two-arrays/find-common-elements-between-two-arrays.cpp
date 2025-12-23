6c26b44d03e8fc6abf4370bd8b2e11fd14867e74
CHINTADA SASI KUMAR <sasikumarchintada64@gmail.com>
2025-12-23 11:42:07 +0530

    Time: 23 ms (18.55%) | Memory: 72 MB (5.47%) - LeetSync

--- 3206-find-common-elements-between-two-arrays/find-common-elements-between-two-arrays.cpp
+++ 3206-find-common-elements-between-two-arrays/find-common-elements-between-two-arrays.cpp
@@ -1,27 +1,29 @@
 class Solution {
 public:
     vector<int> findIntersectionValues(vector<int>& nums1, vector<int>& nums2) {
-        vector<int>ans;
-        int cat=0,cnt=0;
-        for(int i=0;i<nums1.size();i++){
-            for(int j=0;j<nums2.size();j++){
-                if(nums1[i]==nums2[j]){
-                    cnt++;
-                    break;
-                }
-            }
+        unordered_map<int,int>umap1;
+        unordered_map<int,int>umap2;
+        for(int i:nums1){
+            umap1[i]++;
         }
-        ans.push_back(cnt);
-        for(int j=0;j<nums2.size();j++){
-            for(int i=0;i<nums1.size();i++){
-                if(nums2[j]==nums1[i]){
-                    cat++;
-                    break;
-                }
+        for(int j:nums2){
+            umap2[j]++;
+        }
+        int cnt=0,cat=0;
+        vector<int>ans;
+        for(auto i:nums1){
+                if(umap2[i]>0) cnt++;
+                //cout<<p.first<<cnt<<endl;
             }
+        ans.push_back(cnt);
+        for(auto &s:nums2){
+            if(umap1[s]>0) cat++;
+           // cout<<s.first<<cat<<"-"<<endl;
         }
         ans.push_back(cat);
         return ans;
-    }
+        }
 };
+
+
 
